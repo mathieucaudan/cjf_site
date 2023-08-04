6dfb90d2f4f1fd7dd4a5db458d0a86a55be72ea9
Pulkio <guillaume.cobat@gmx.fr>
2023-08-04 15:23:11 +0200

    changement bouton et dossier css

--- fonction.php
+++ fonction.php
@@ -10,136 +10,9 @@ function entete(){
         <link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Amatic+SC'>
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+        <link rel='stylesheet' href='style/fonctionCss.css'> <!-- Ajout de la référence au fichier CSS -->
         <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'></script>
 
-         <style>
-            body {margin:0;font-family:Arial}
-
-        .topnav {
-        overflow: hidden;
-        background-color: rgb(32, 47, 74);
-        }
-
-        .topnav a {
-        float: left;
-        display: block;
-        color: #f2f2f2;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-        font-size: 17px;
-        }
-
-        .active {
-        background-color: #04AA6D;
-        color: white;
-        }
-
-        .topnav .icon {
-        display: none;
-        }
-
-        .dropdown {
-        float: left;
-        overflow: hidden;
-        }
-
-        .dropdown .dropbtn {
-        font-size: 17px;    
-        border: none;
-        outline: none;
-        color: white;
-        padding: 14px 16px;
-        background-color: inherit;
-        font-family: inherit;
-        margin: 0;
-        }
-
-        .dropdown-content {
-        display: none;
-        position: absolute;
-        background-color: #f9f9f9;
-        min-width: 160px;
-        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-        z-index: 1;
-        }
-
-        .dropdown-content a {
-        float: none;
-        color: black;
-        padding: 12px 16px;
-        text-decoration: none;
-        display: block;
-        text-align: left;
-        }
-
-        .topnav a:hover, .dropdown:hover .dropbtn {
-        background-color: #555;
-        color: white;
-        }
-
-        .dropdown-content a:hover {
-        background-color: #ddd;
-        color: black;
-        }
-
-        .dropdown:hover .dropdown-content {
-        display: block;
-        }
-
-        @media screen and (max-width: 600px) {
-        .topnav a:not(:first-child), .dropdown .dropbtn {
-            display: none;
-        }
-        .topnav a.icon {
-            float: right;
-            display: block;
-        }
-        }
-
-        @media screen and (max-width: 600px) {
-        .topnav.responsive {position: relative;}
-        .topnav.responsive .icon {
-            position: absolute;
-            right: 0;
-            top: 0;
-        }
-        .topnav.responsive a {
-            float: none;
-            display: block;
-            text-align: left;
-        }
-        .topnav.responsive .dropdown {float: none;}
-        .topnav.responsive .dropdown-content {position: relative;}
-        .topnav.responsive .dropdown .dropbtn {
-            display: block;
-            width: 100%;
-            text-align: left;
-        }
-        button {
-            width: 150px;
-            height: 40px;
-            background-color: #FFFFFF;
-            color: #FFFFFF
-            font-size: 10px;
-            display: inline-block;
-            border-radius: 5px;
-            border: 4px double #00000;
-            text-align: center;
-            -webkit-transition: all 0.5s;
-            -moz-transition: all 0.5s;
-            -o-transition: all 0.5s;
-            transition: all 0.5s;
-            cursor: pointer;
-            margin: 5px; 
-        }
-        .w3-button-container {
-            display: flex;
-            align-items: center;
-        }
-        }
-
-        </style>
      </head>";
     }
 function footer() {
@@ -180,9 +53,9 @@ function navbar() {
     </div>";
     echo "<img id='logo' src='image/logo_cjf.png' style='float: right; margin-top: 10px;'>";
     if (isset($_SESSION['role'])) {
-        echo "<a href='deconnexion.php' class='w3-right'><button  style='float: right' type='button'>Déconnexion</button></a>";
+        echo "<a href='deconnexion.php' class='buttonContainer w3-right '><button class='myButton'  style='float: right' type='button'>Déconnexion</button></a>";
     } else {
-        echo "<a href='connexion.php' class='w3-right'><button style='float: right' type='button'>Connexion</button></a>";
+        echo "<a href='connexion.php' class='buttonContainer w3-right'><button class='myButton' style='float: right' type='button'>Connexion</button></a>";
     }
     echo "
     <script>
