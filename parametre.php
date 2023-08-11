723d712d6d38b6f92199e873f2b8f68d74048271
mathieucaudan <mathieu.caudan@etudiant.univ-rennes1.fr>
2023-08-11 21:28:07 +0200

    changement parametres.php et remise en place d'ajout d'image carousel

--- parametre.php
+++ parametre.php
@@ -3,46 +3,102 @@ include 'fonction.php';
 entete();
 navbar();
 echo "<body style='background-color: rgb(32, 47, 74);'>";
+?>
+
+<div class="tab" style='background-color: rgb(32, 47, 74); font-size: 40px'><center>
+<h1 style='color: white;'>Record</h1>
+  <button class="tablinks" style='background-color: rgb(32, 47, 74); color: white' onclick="openTab(event, 'ajoutarticle')">Ajouter un article</button>
+  <button class="tablinks" style='background-color: rgb(32, 47, 74); color: white' onclick="openTab(event, 'supparticle')">Supprimer un article</button>
+  <button class="tablinks" style='background-color: rgb(32, 47, 74); color: white' onclick="openTab(event, 'ajoutimagecarousel')">Ajouter une image</button>
+  <button class="tablinks" style='background-color: rgb(32, 47, 74); color: white' onclick="openTab(event, 'suppimagecarousel')">Supprimer une image</button>
+  <button class="tablinks" style='background-color: rgb(32, 47, 74); color: white' onclick="openTab(event, 'changerecord')">Changer un record</button>
+</center></div>
+
+<div id="ajoutarticle" class="tabcontent" style='background-color: rgb(32, 47, 74); color: white; margin: 0 auto;'>
+  <table>
+  <?php
+    ajoutarticle();
+    ?>
+  </table>
+</div>
+
+<div id="supparticle" class="tabcontent" style='background-color: rgb(32, 47, 74); color: white; margin: 0 auto;'>
+  <table>
+  <?php
+    supparticle();
+    ?>
+  </table>
+</div>
+
+<div id="ajoutimagecarousel" class="tabcontent" style='background-color: rgb(32, 47, 74); color: white; margin: 0 auto;'>
+  <table>
+  <?php
+    ajoutimagecarousel();
+    ?>
+  </table>
+</div>
+
+<div id="suppimagecarousel" class="tabcontent" style='background-color: rgb(32, 47, 74); color: white; margin: 0 auto;'>
+  <table>
+  <?php
+    suppimagecarousel();
+    ?>
+  </table>
+</div>
+
+<div id="changerecord" class="tabcontent" style='background-color: rgb(32, 47, 74); color: white; margin: 0 auto;'>
+  <table>
+  <?php
+    changerecord();
+    ?>
+  </table>
+</div>
 
 
 
-
-if(isset($_GET['action'])) {
-    $action = $_GET['action'];
-    echo "<button onclick='goBack()' style='font-size: 24px; color: white; background-color: rgb(32, 47, 74);'>Retour</button>";
-    if($action == 'ajoutimagecarousel') {
-        ajoutimagecarousel();
-    } elseif($action == 'suppimagecarousel') {
-        suppimagecarousel();
-    }elseif($action == 'ajoutarticle') {
-        ajoutarticle();
-    } elseif($action == 'supparticle') {
-        supparticle();
-    }elseif($action == 'changerecord') {
-        changerecord();
-    }
-    
-} else {
-    echo "<center id='actionLinks'>
-    <a href='parametre.php?action=ajoutimagecarousel' style='font-size: 24px; color: white;' onclick='hideLinks();'>Ajouter une image au Carousel</a><br>
-    <a href='parametre.php?action=suppimagecarousel' style='font-size: 24px; color: white;' onclick='hideLinks();'>Supprimer une image du Carousel</a><br>
-    <a href='parametre.php?action=ajoutarticle' style='font-size: 24px; color: white;' onclick='hideLinks();'>Ajouter un article</a><br>
-    <a href='parametre.php?action=supparticle' style='font-size: 24px; color: white;' onclick='hideLinks();'>Supprimer un article</a><br>
-    <a href='parametre.php?action=changerecord' style='font-size: 24px; color: white;' onclick='hideLinks();'>Modifier les Records</a><br>
-    </center>";
-
-}
-
-
+<?php
 footer();
 echo "</body>";
 ?>
+<style>
+    .tab {
+  overflow: hidden;
+}
+
+.tab button {
+  background-color: #ccc;
+  border: none;
+  color: black;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.tab button:hover {
+  background-color: #ddd;
+}
+
+.tab button.active {
+  background-color: #fff;
+}
+
+.tabcontent {
+  display: none;
+  padding: 20px;
+  border: 1px solid #ccc;
+}
+</style>
 <script>
-    function hideLinks() {
-        var links = document.getElementById("actionLinks");
-        links.style.display = "none";
-    }
-    function goBack() {
-        window.history.back();
-    }
+    function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 </script>
