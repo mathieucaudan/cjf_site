850361289b28960872e388ea33f126b146e62dfd
mathieucaudan <mathieu.caudan@etudiant.univ-rennes1.fr>
2023-08-27 22:29:30 +0200

    carousel dynamique

--- accueil.php
+++ accueil.php
@@ -2,169 +2,134 @@
 include 'fonction.php';
 entete();
 navbar();
-
-echo "<body>";
 ?>
 
-<div class="CSSgal">
-
-  <!-- Don't wrap targets in parent -->
-  <s id="s1"></s> 
-  <s id="s2"></s>
-  <s id="s3"></s>
-  <s id="s4"></s>
-
-
-  <div class="slider">
-    <?php
-    $dir = 'image/carousel'; // Remplacez "chemin/vers/le/dossier" par le chemin réel vers votre dossier d'images
-
-    // Récupérer la liste des fichiers dans le dossier
-    $files = scandir($dir);
-
-    // Filtrer les fichiers pour ne garder que les images
-    $images = array_filter($files, function($file) {
-        $extension = pathinfo($file, PATHINFO_EXTENSION);
-        $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif']; // Ajoutez d'autres extensions d'images si nécessaire
-        return in_array($extension, $allowedExtensions);
-    });
-
-    // Afficher les images dans le carousel
-    foreach ($images as $image) {
-        echo '<div>';
-        echo '<img class="custom-img" src="' . $dir . '/' . $image . '" alt="Image">';
-        echo '</div>';
-    }
-    ?>
-  </div>
-  
-</div>
-
-<style>
-*{box-sizing: border-box; -webkit-box-sizing: border-box; }
+<body>
+    <style>
+      *{box-sizing: border-box; -webkit-box-sizing: border-box; }
 html, body { height: 100%; }
 body { margin: 0; font: 16px/1.3 sans-serif; }
 
-.CSSgal {
-  position: relative;
-  overflow: hidden;
-  height: auto; /* Utiliser "auto" pour s'adapter à la hauteur des images */
-  max-height: 80%; /* Limiter la hauteur maximale à 100% de la fenêtre */
-}
-
-/* SLIDER */
-
-.CSSgal .slider {
-  height: auto;
-  white-space: nowrap;
-  font-size: 0;
-  transition: 0.8s;
-  width: 100%; /* Ajustez la largeur comme nécessaire */
-  display: flex;
-}
-
-/* SLIDES */
-
-.CSSgal .slider > * {
-  font-size: 1rem;
-  display: inline-block;
-  white-space: normal;
-  vertical-align: top;
-  /*height: 100%;*/
-  background: none 50% no-repeat;
-  background-size: cover;
-  flex: 0 0 100%; /* Chaque slide occupe 100% de la largeur du slider */
-  transition: transform 0.8s;
-}
-
-/* SLIDER ANIMATION POSITIONS */
-
-#s1:target ~ .slider {transform: translateX(0%); -webkit-transform: translateX(0%);}
-#s2:target ~ .slider {transform: translateX(-100%); -webkit-transform: translateX(-100%);}
-#s3:target ~ .slider {transform: translateX(-200%); -webkit-transform: translateX(-200%);}
-#s4:target ~ .slider {transform: translateX(-300%); -webkit-transform: translateX(-300%);}
-
-
-
-@keyframes slideAnimation {
-  0%, 100% {
-    transform: translateX(0%);
-  }
-  20% {
-    transform: translateX(0%);
-  }
-  40% {
-    transform: translateX(-101%);
-  }
-  60% {
-    transform: translateX(-101%);
-  }
-  80% {
-    transform: translateX(-202%);
-  }
-}
-
-.CSSgal .slider {
-  animation: slideAnimation 15s infinite; /* La valeur "15s" est la durée de l'animation, ajustez-la selon vos besoins */
-}
-
-</style>
-
-
-<script>
-// Fonction pour ajuster la taille des images en fonction de la plus grande image
-// Fonction pour ajuster la taille des images en fonction de la plus petite image
-function adjustImageSizes() {
-  const images = document.querySelectorAll('.custom-img');
-  const windowWidth = window.innerWidth;
-
-  // Trouver la hauteur minimale à partir des dimensions de la plus petite image
-  let minHeight = Number.MAX_SAFE_INTEGER;
-
-  images.forEach((image) => {
-    const originalWidth = image.naturalWidth;
-    const originalHeight = image.naturalHeight;
-    const newWidth = windowWidth;
-    const newHeight = (originalHeight / originalWidth) * newWidth;
-
-    if (newHeight < minHeight) {
-      minHeight = newHeight;
-    }
-  });
-
-  // Ajuster toutes les images en fonction de la hauteur minimale
-  images.forEach((image) => {
-    const originalWidth = image.naturalWidth;
-    const originalHeight = image.naturalHeight;
-    const newWidth = windowWidth;
-    const newHeight = (originalHeight / originalWidth) * newWidth;
-
-    image.style.width = `${newWidth}px`;
-    image.style.height = `${minHeight}px`;
-  });
+.slider-container {
+    position: relative;
+    overflow: hidden;
+    height: auto;
+    max-height: 80%;
+    display: flex; /* Ajout pour centrer verticalement et horizontalement */
+    align-items: center; /* Ajout pour centrer verticalement */
+    justify-content: center; /* Ajout pour centrer horizontalement */
 }
 
 
+        /* SLIDER */
+        .slider-container .slider {
+            height: auto;
+            white-space: nowrap;
+            font-size: 0;
+            transition: 2s;
+            width: 100%; /* Ajustez la largeur comme nécessaire */
+            display: flex;
+            /* Pas besoin d'animation CSS ici */
+        }
 
-// Fonction pour charger une image et retourner une promesse
-function loadImage(src) {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.onerror = reject;
-    img.src = src;
-  });
-}
+        /* SLIDES */
+        .slider-container .slider > * {
+            font-size: 1rem;
+            display: inline-block;
+            white-space: normal;
+            vertical-align: top;
+            background: none 50% no-repeat;
+            background-size: cover;
+            flex: 0 0 100%; /* Chaque slide occupe 100% de la largeur du slider */
+            transition: transform 2s;
+        }
 
-// Appel initial pour ajuster les tailles d'images au chargement de la page
-window.addEventListener('load', adjustImageSizes);
+        /* Ancien CSS pour ajuster la taille des images */
+        .custom-img {
+            width: 100%;
+            height: auto;
+        }
+        
+    </style>
+    <div class="slider-container">
+        <div class="slider">
+            <?php
+            $imageFolder = "image/carousel/";
+            $images = glob($imageFolder . "*.{jpg,png,gif,JPG,Jpg}", GLOB_BRACE);
 
-// Appel pour ajuster les tailles d'images lors du redimensionnement de la fenêtre
-window.addEventListener('resize', adjustImageSizes);
-</script>
+            foreach ($images as $image) {
+                echo "<img src='$image' alt='Image' class='custom-img'>";
+            }
+            ?>
+        </div>
+    </div>
+    <script>
+        // Fonction pour ajuster la taille des images en fonction de la plus grande image
+        // Fonction pour ajuster la taille des images en fonction de la plus petite image
+        function adjustImageSizes() {
+            const images = document.querySelectorAll('.custom-img');
+            const windowWidth = window.innerWidth;
 
+            // Trouver la hauteur minimale à partir des dimensions de la plus petite image
+            let minHeight = Number.MAX_SAFE_INTEGER;
+
+            images.forEach((image) => {
+                const originalWidth = image.naturalWidth;
+                const originalHeight = image.naturalHeight;
+                const newWidth = windowWidth;
+                const newHeight = (originalHeight / originalWidth) * newWidth;
+
+                if (newHeight < minHeight) {
+                    minHeight = newHeight;
+                }
+            });
+
+            // Ajuster toutes les images en fonction de la hauteur minimale
+            images.forEach((image) => {
+                const originalWidth = image.naturalWidth;
+                const originalHeight = image.naturalHeight;
+                const newWidth = windowWidth;
+                const newHeight = (originalHeight / originalWidth) * newWidth;
+
+                image.style.width = `${newWidth}px`;
+                image.style.height = `${minHeight}px`;
+            });
+        }
+
+        // Fonction pour charger une image et retourner une promesse
+        function loadImage(src) {
+            return new Promise((resolve, reject) => {
+                const img = new Image();
+                img.onload = () => resolve(img);
+                img.onerror = reject;
+                img.src = src;
+            });
+        }
+
+        // Appel initial pour ajuster les tailles d'images au chargement de la page
+        window.addEventListener('load', adjustImageSizes);
+
+        // Appel pour ajuster les tailles d'images lors du redimensionnement de la fenêtre
+        window.addEventListener('resize', adjustImageSizes);
+
+        // Fonction pour faire défiler automatiquement les images
+        function startSlider() {
+            let currentIndex = 0;
+            const images = document.querySelectorAll('.custom-img');
+            const numImages = images.length;
+
+            setInterval(() => {
+                currentIndex = (currentIndex + 1) % numImages;
+                const translateValue = `translateX(-${currentIndex * 100}%)`;
+                document.querySelector('.slider').style.transform = translateValue;
+            }, 3000); // Change l'image toutes les 3 secondes
+        }
+
+        // Appeler la fonction pour démarrer le slider automatique
+        startSlider();
+    </script>
+</body>
 
 <?php
 footer();
-echo "</body>";
 ?>
