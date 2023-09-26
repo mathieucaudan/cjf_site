7994c45fa527f16325d6d260635b746bbf091779
mathieucaudan <mathieu.caudan@etudiant.univ-rennes1.fr>
2023-09-26 23:47:28 +0200

    avenir

--- section.php
+++ section.php
@@ -16,7 +16,7 @@ navbar();
     <div class="tab" style='background-color: rgb(32, 47, 74); font-size: 40px'>
         <button class="tablinks presentation active" style='background-color: rgb(32, 47, 74); color: white' onclick="openTab(event, 'presentation')">Présentation</button>
         <button class="tablinks article" style='background-color: rgb(32, 47, 74); color: white' onclick="openTab(event, 'article')">article</button>
-        <button class="tablinks avenir" style='background-color: rgb(32, 47, 74); color: white' onclick="openTab(event, 'avenir')">Génération Avenir</button>
+
     </div>
 </center>
 
