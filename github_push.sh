c5abee5f536ae3b6ef467a1a728757e2da87c3c8
mathieucaudan <mathieu.caudan@etudiant.univ-rennes1.fr>
2023-09-26 21:26:42 +0200

    maj script

--- github_push.sh
+++ github_push.sh
@@ -1,5 +1,29 @@
-#!/bin/bash
-PROJECT_DIR ="/public_html"
-cd $PROJECT_DIR
-git pull origi master
-git push origin master
+#!/usr/bin/expect
+
+set username "mathieucaudan"
+set password "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"  ;# Remplacez par votre mot de passe GitHub
+set commit_message "maj"
+
+# Exécutez la commande git add *
+spawn git add *
+
+# Exécutez la commande git commit -m "maj"
+spawn git commit -m "$commit_message"
+
+# Exécutez la commande git push
+spawn git push
+
+# Attendez que le programme demande le nom d'utilisateur
+expect "Username for 'https://github.com':"
+
+# Répondez avec le nom d'utilisateur
+send "$username\r"
+
+# Attendez que le programme demande le mot de passe
+expect "Password for 'https://$username@github.com':"
+
+# Répondez avec le mot de passe
+send "$password\r"
+
+# Attendez la fin du programme
+expect eof
