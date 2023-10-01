e193381aba568ae1fd2e378e449003ba8d81c29d
mathieucaudan <mathieu.caudan@etudiant.univ-rennes1.fr>
2023-10-01 21:58:07 +0200

    maj google

--- fonction.php
+++ fonction.php
@@ -6,9 +6,9 @@ function entete()
     <html>
     <head>
         <title>CJF Saint-Malo Pentathlon Moderne</title>
-        <meta name='description' content='CJF Saint-Malo Pentathlon Moderne'>
         <meta charset='UTF-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <meta name='description' content='Section Pentathlon Moderne du Cercle Jules Ferry Saint-Malo, LaserRun / Natation / Escrime / Obstacle'>
         <link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Amatic+SC'>
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
