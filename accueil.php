82d645bd542b123b60d54ce9426b579477e4fb00
mathieucaudan <mathieu.caudan@etudiant.univ-rennes1.fr>
2023-10-07 15:28:50 +0200

    maj accueil

--- accueil.php
+++ accueil.php
@@ -25,21 +25,26 @@ $data = json_decode($jsonData, true);
     <center>
       <h1>Accueil</h1>
     </center>
-
     <div class="w3-container w3-twothird w3-center">
-      <h2>Qu'est-ce que le</h2>
-      <h2>Pentathlon Moderne ?</h2>
-      <p>Le pentathlon moderne est une compétition sportive exigeante qui combine cinq disciplines distinctes pour couronner un champion. Ces épreuves sont soigneusement conçues pour refléter les compétences essentielles d'un soldat d'élite. Les athlètes rivalisent pour obtenir le meilleur score global en combinant leurs performances dans ces cinq disciplines.</p>
+      <h1>Article a la une</h1>
+      <div class='art'>
+        <?php
+        $dossierPdf = './article/article_pdf';
+        $dossierImage = './article/article_image';
+        $dossierJson = './article/article_json/article.json';
 
-      <p>La première épreuve consiste en l'escrime, où les compétiteurs s'affrontent en duel à l'épée. L'objectif est de toucher l'adversaire tout en évitant d'être touché, et des points sont attribués en fonction des touches réussies.</p>
+        $jsonData = file_get_contents($dossierJson);
+        $article = array_reverse(json_decode($jsonData, true));
 
-      <p>La natation représente la deuxième épreuve, où les athlètes doivent parcourir une distance de 200 mètres en utilisant le style libre. Les temps de nage sont enregistrés et convertis en points en fonction des performances individuelles.</p>
-
-      <p>La troisième discipline est l'équitation, plus précisément le saut d'obstacles. Chaque athlète se voit attribuer un cheval par tirage au sort et dispose d'un court laps de temps pour se familiariser avec le cheval. L'objectif est de parcourir un parcours d'obstacles sans commettre de faute dans un temps limité.</p>
-
-      <p>La dernière épreuve est un combiné appelé Laser Run, le Laser Run, est une combinaison unique de tir au pistolet et de course à pied. Les athlètes utilisent des pistolets laser pour viser des cibles situées à 10 mètres de distance, alternant avec des courses, et les scores sont déterminés par la précision des tirs et le temps de course.</p>
-
-      <p>Le champion du pentathlon moderne est celui qui parvient à combiner habilement ses performances dans chacune de ces disciplines pour obtenir le score total le plus élevé. Ce sport, devenu une épreuve olympique en 1912, exige une polyvalence exceptionnelle et une grande capacité d'adaptation de la part des athlètes pour exceller dans l'ensemble de ces épreuves variées.</p>
+        echo "<a class='artcard' href='$dossierPdf/" . pathinfo($article[0]['image'], PATHINFO_FILENAME) . ".pdf' target='_blank'>
+        <div>
+          <h2>{$article[0]['titre']}</h2>
+          <p>{$article[0]['description']}</p>
+          <div>{$article[0]['date']}</div>
+        </div>
+        <img src='$dossierImage/{$article[0]['image']}' class='titre-img'>";
+        ?>
+      </div>
     </div>
     <div class="w3-container w3-third w3-center">
       <h2>Derniers résultats/infos</h2>
